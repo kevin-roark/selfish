ade29058e30a0127f8103dacd051e9fb82b0324a
Kevin Roark <ker2143@columbia.edu>
2016-09-13 14:51:07 -0400

    initial manikinning

---
+++ manikin/manikin.js
@@ -0,0 +1,30 @@
+(function () {
+  var content = [
+    { image: '3', text: 'Husband and Wife and the Muscular Horse they Got' },
+    { image: '5', text: 'Man Looking at Women Not Looking at the Family Separated by the Window' },
+    { image: '2', text: 'Groomed and Bearded Forms Divided by the Reflection of My Pants' },
+    { image: '7', text: 'One Pack of Men behind the Glass and another Pack in front of it' },
+    { image: '4', text: 'The Car in the Woman on the Horse and her Daughter' },
+    { image: '6', text: 'Woman in Pink and Woman in Blue and Woman in Yellow seen through the Mirror made of Buildings' },
+    { image: '8', text: 'One Man and One Woman and One Woman and One Man for Sale in front of a TV' },
+
+    { image: '1', text: 'Four Women on a Platform through a Window and Three are in Love' },
+
+    { image: '9', text: 'Sixteen Male Heads and Four and One Half Male Torsos each on a Pedestal' },
+    { image: '10', text: 'Growth Curve where the Points are Bodies' },
+
+    { image: '11', text: 'One Man Alone in the Dark' },
+    { image: '12', text: 'Circle of Men Staring at Pants and a Body as Seen from the Street' },
+    { image: '13', text: 'Old Man Trapped in the Cage with a Curtain and a Painting and his Family' },
+    { image: '14', text: 'Woman Without Wig Working for Woman with Wig by a Hollow Cross' },
+
+    { image: '15', text: 'Fourteen Bodies Arranged for an Ad in a Window on a Building near a Truck' },
+    { image: '18', text: 'Litter of Virgin Births from Deep Earth and a Visor with a Price Tag' },
+    { image: '20', text: 'Sunglasses on a Rod in the Reflection of a Car in the Window surrounded by Perfect Bodies and the Equipment that Constrains Them' },
+
+    { image: '16', text: 'Three Hands Grasping for Red Natal Light in an Upward and Wrong Direction' },
+    { image: '19', text: 'Hands Clutching at Their Own Insectile Reflections' },
+    { image: '17', text: 'Yellow Minivan Taxi Driving to a Man with Strange Light and Strong body and Deformed Genitals and the Hands' },
+    { image: '21', text: 'NEW Hands Clawing at the Sky or the Roof and a Honda Parked Alone' }
+  ];
+})();
