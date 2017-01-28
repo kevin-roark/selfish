9e8a4a0f869e847ab0c235f930ad2666f5cfb55b
Kevin Roark <ker2143@columbia.edu>
2017-01-27 21:56:21 -0500

    vue-cli webpack scaffold

---
+++ vue-kevin/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+html, body {
+  margin: 0; padding: 0;
+  width: 100%; height: 100%;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
