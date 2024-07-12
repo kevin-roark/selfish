a50c90121a3e50f5457c6a73c3458f3d6f5e49b2
Kevin Roark <kevin.e.roark@gmail.com>
2024-07-12 16:34:09 -0400

    include previous subsites in public dir

--- src/util/is-touch.js
+++
@@ -1,8 +0,0 @@
-function isTouchDevice() {
-  return 'ontouchstart' in window // works on most browsers
-    || navigator.maxTouchPoints;  // works on IE10/11 and Surface
-}
-
-const touch = isTouchDevice();
-
-export default touch;
